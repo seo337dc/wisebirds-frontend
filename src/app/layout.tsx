644f3eaef8dfc09d464614dc8ca365c6fb94bceb
seo337dc <sdc337dc@gmail.com>
2025-02-13 15:25:02 +0900

    init : react-query 세팅

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,5 @@
+import QueryProvider from "@/util/QueryClientProvider";
+
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -27,7 +29,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <QueryProvider>{children}</QueryProvider>
       </body>
     </html>
   );
