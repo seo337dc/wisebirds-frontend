3aeb6ee340ad26417d079f142822adf8cf1c89e2
seo337dc <sdc337dc@gmail.com>
2025-02-13 16:06:59 +0900

    fix : styled-component 에러관련 수정

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  compiler: {
+    styledComponents: true, // SSR styled-component 적용
+  },
 };
 
 export default nextConfig;
