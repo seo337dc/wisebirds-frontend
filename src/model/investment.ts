11758faa19179f99331836f03b31424132235771
seo337dc <sdc337dc@gmail.com>
2025-02-20 19:33:07 +0900

    fix : invest 용어에서 asset로 모두 변경

--- src/model/investment.ts
+++
@@ -1,9 +0,0 @@
-export type ModelInvestment = {
-  id: number; // 2;
-  diary_id: number; // 2;
-  asset_id: number; // 1;
-  amount: string; // "10.0";
-  buy_price: string; // "100.0";
-  created_at: string; // "2020-11-24T20:33:40.541Z";
-  updated_at: string; // "2020-11-24T20:33:40.541Z";
-};
