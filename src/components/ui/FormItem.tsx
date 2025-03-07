07afef7d6278fc283f29aa95c6cc3e0e35c2ffdf
seo337dc <sdc337dc@gmail.com>
2025-03-07 20:52:43 +0900

    add : 사용자 수정 모달 화면 및 기능 개발

---
+++ src/components/ui/FormItem.tsx
@@ -0,0 +1,8 @@
+import { Form } from "antd";
+import { styled } from "styled-components";
+
+export const FormItem = styled(Form.Item)`
+  .ant-form-item-explain-error {
+    margin-top: 8px;
+  }
+`;
