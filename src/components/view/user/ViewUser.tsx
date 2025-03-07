7477b1c403bad93fb837aac5ba6acb82e47bb99c
seo337dc <sdc337dc@gmail.com>
2025-03-07 20:02:45 +0900

    add : 사용자 페이지 화면 개발 및 api 목록 노출

---
+++ src/components/view/user/ViewUser.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import { useState } from "react";
+import TableSection from "./TableSection";
+
+const ViewUser = () => {
+  const [page, setPage] = useState(0);
+  const handlePage = (pageValue) => setPage(pageValue);
+
+  return (
+    <div>
+      <TableSection page={page} size={25} handlePage={handlePage} />
+    </div>
+  );
+};
+
+export default ViewUser;
