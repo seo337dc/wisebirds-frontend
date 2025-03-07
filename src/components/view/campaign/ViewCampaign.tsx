794735bcb4ece825852ba2e855c8a5e24ba54ec3
seo337dc <sdc337dc@gmail.com>
2025-03-07 20:03:12 +0900

    ect : 기타 개발

--- src/components/view/campaign/ViewCampaign.tsx
+++ src/components/view/campaign/ViewCampaign.tsx
@@ -3,15 +3,13 @@
 import { useState } from "react";
 import TableSection from "./TableSection";
 
-const size = 25;
-
 const ViewCampaign = () => {
   const [page, setPage] = useState(0);
   const handlePage = (pageValue) => setPage(pageValue);
 
   return (
     <div>
-      <TableSection page={page} size={size} handlePage={handlePage} />
+      <TableSection page={page} size={25} handlePage={handlePage} />
     </div>
   );
 };
